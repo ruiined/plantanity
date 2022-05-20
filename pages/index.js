1b108368db02b5e4e85a466678f402db15daac33
ruiined <ruiined@gmail.com>
2022-05-21 00:10:40 +0100

    Basic functional main page is done

--- pages/index.js
+++ pages/index.js
@@ -44,82 +44,83 @@ const Home = () => {
         <meta name="description" content="Grow your dreams" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
+      <header>
+        <div className="bg-white shadow-lg max-w-6xl mx-auto px-4 flex justify-between flex space-x-7 stickyl">
+          <h1 className="flex items-center py-4 px-2 font-semibold text-gray-700 text-lg">
+            <a href="/">Plantanity</a>
+          </h1>
+          {/* navigation items */}
+          <div className="hidden md:flex items-center space-x-1">
+            <a
+              href=""
+              class="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold hover:bg-green-100"
+            >
+              Garden
+            </a>
+            <a
+              href=""
+              class="py-4 px-2 text-gray-400 border-b-4 border-gray-400 font-semibold hover:bg-gray-100"
+            >
+              Seeds
+            </a>
+            <a
+              href=""
+              class="py-4 px-2 text-gray-400 border-b-4 border-gray-400 font-semibold hover:bg-gray-100"
+            >
+              Water
+            </a>
+            <a
+              href=""
+              class="py-4 px-2 text-gray-400 border-b-4 border-gray-400 font-semibold hover:bg-gray-100"
+            >
+              Achievements
+            </a>
+          </div>
+          <div className="hidden md:flex items-center space-x-3 ">
+            <a
+              href=""
+              className="py-2 px-2 font-medium text-gray-500 rounded hover:bg-green-500 hover:text-white transition duration-300"
+            >
+              Log In
+            </a>
+            <a
+              href=""
+              className="py-2 px-2 font-medium text-white bg-green-500 rounded hover:bg-green-400 transition duration-300"
+            >
+              Sign Up
+            </a>
+          </div>
+        </div>
+      </header>
 
-      <main className="">
-        <div>
-          {/* navigation bar */}
-          <div className="bg-white shadow-lg max-w-6xl mx-auto px-4 flex justify-between flex space-x-7">
-            <h1 className="flex items-center py-4 px-2 font-semibold text-gray-700 text-lg">
-              <a href="/">Plantanity</a>
-            </h1>
-            {/* navigation items */}
-            <div className="hidden md:flex items-center space-x-1">
-              <a
-                href=""
-                class="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold hover:bg-green-100"
-              >
-                Garden
-              </a>
-              <a
-                href=""
-                class="py-4 px-2 text-gray-400 border-b-4 border-gray-400 font-semibold hover:bg-gray-100"
-              >
-                Seeds
-              </a>
-              <a
-                href=""
-                class="py-4 px-2 text-gray-400 border-b-4 border-gray-400 font-semibold hover:bg-gray-100"
-              >
-                Water
-              </a>
-              <a
-                href=""
-                class="py-4 px-2 text-gray-400 border-b-4 border-gray-400 font-semibold hover:bg-gray-100"
-              >
-                Achievements
-              </a>
-            </div>
-            <div className="hidden md:flex items-center space-x-3 ">
-              <a
-                href=""
-                className="py-2 px-2 font-medium text-gray-500 rounded hover:bg-green-500 hover:text-white transition duration-300"
-              >
-                Log In
-              </a>
-              <a
-                href=""
-                className="py-2 px-2 font-medium text-white bg-green-500 rounded hover:bg-green-400 transition duration-300"
-              >
-                Sign Up
-              </a>
-            </div>
-          </div>
-          {/* main content */}
-          {loading ? (
-            <img src="/loader.gif" />
-          ) : (
-            <form className="" onSubmit={addTask}>
-              <input
-                className=""
-                type="text"
-                name="task"
-                onChange={changeHandler}
-                placeholder="Enter your task here!"
-              />
-            </form>
-          )}
-          <div className="columns-3xs gap-8">
-            {data.map((item) => (
-              <a
-                href="#"
-                key={item.key}
-                className="w-full aspect-video"
-                onClick={() => removeTask(item._id)}
-              >
-                <p>{item.title}</p>
-              </a>
-            ))}
-          </div>
+      <main className="w-full h-full flex-grow p-3 overflow-auto">
+        <div>{/* navigation bar */}</div>
+        {/* main content */}
+        {loading ? (
+          <img src="/loader.gif" />
+        ) : (
+          <form className="" onSubmit={addTask}>
+            <input
+              className=""
+              type="text"
+              name="task"
+              onChange={changeHandler}
+              placeholder="Enter your task here!"
+            />
+          </form>
+        )}
+        <div className="columns-3xs gap-8">
+          {data.map((item) => (
+            <a
+              href="#"
+              key={item.key}
+              className="w-full aspect-video"
+              onClick={() => removeTask(item._id)}
+            >
+              <p>{item.title}</p>
+            </a>
+          ))}
         </div>
       </main>
 
