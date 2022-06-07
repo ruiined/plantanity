b98e8d5b5d42cdc35819a58b87f0d1b875d6bf1f
ruiined <ruiined@gmail.com>
2022-06-07 23:47:44 +0100

    Options hidden until the task is on hover

--- components/tasks/task.tsx
+++ components/tasks/task.tsx
@@ -4,7 +4,7 @@ import { Dropdown } from "./task/dropdown";
 
 export function Task({ task, editTask, removeTask, completeTask }) {
   return (
-    <li className="flex transition duration-300 hover:bg-gray-50 h-16 p-3 rounded-md text-[15px] items-center">
+    <li className="hover flex transition duration-300 hover:bg-gray-50 h-16 p-3 rounded-md text-[15px] items-center">
       <Checkbox completeTask={completeTask} task={task} />
       <div
         contentEditable
--- components/tasks/task/dropdown.tsx
+++ components/tasks/task/dropdown.tsx
@@ -6,7 +6,7 @@ export const Dropdown = ({ removeTask, task }) => {
       <label tabIndex={0}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 text-gray-300 hover:text-green-500 hover:cursor-pointer"
+          className="show-on-hover h-6 w-6 text-gray-300 hover:text-green-500 hover:cursor-pointer"
           fill="none"
           viewBox="0 0 30 30"
           stroke="currentColor"
