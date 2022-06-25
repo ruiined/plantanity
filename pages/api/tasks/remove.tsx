e979ee257b43d90f785b746e3c5d39c37123a742
ruiined <ruiined@gmail.com>
2022-06-25 22:47:52 +0100

    Fixed the post api disfunction

--- pages/api/tasks/remove.tsx
+++ pages/api/tasks/remove.tsx
@@ -4,9 +4,8 @@ import { connectDB } from "@lib/db";
 import Task from "@models/task";
 
 const removeTask = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { body } = req;
-  if (!body.query.task) res.status(400).send("Task parameter required");
-  let task = encodeURI(body.query.task);
+  if (!req.query.task) res.status(400).send("Task parameter required");
+  let task = encodeURI(req.query.task);
   await connectDB();
   const tasks = await Task.deleteOne({ _id: new ObjectId(task) });
   res.status(200).json({ tasks });
