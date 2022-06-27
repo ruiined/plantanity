509ba24490894b61b0209f471e5996e62f323b53
ruiined <ruiined@gmail.com>
2022-06-27 22:05:16 +0100

    Uncommented the unit test

--- tests/pages/tasks.test.tsx
+++ tests/pages/tasks.test.tsx
@@ -3,26 +3,26 @@ import { RecoilRoot } from "recoil";
 import "@testing-library/jest-dom";
 import { Tasks } from "@pages/tasks";
 
-// describe("Tasks", () => {
-//   test("task input", () => {
-//     render(
-//       <RecoilRoot>
-//         <Tasks />
-//       </RecoilRoot>
-//     );
+describe("Tasks", () => {
+  test("task input", () => {
+    render(
+      <RecoilRoot>
+        <Tasks />
+      </RecoilRoot>
+    );
 
-//     const input = screen.queryByTestId("task-input");
+    const input = screen.queryByTestId("task-input");
 
-//     expect(input).toBeInTheDocument();
-//     expect(input).toHaveAttribute("type", "text");
-//     expect(input).toHaveAccessibleName();
-//     expect(input.value).toBe("");
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveAttribute("type", "text");
+    expect(input).toHaveAccessibleName();
+    expect(input.value).toBe("");
 
-//     fireEvent.change(input, { target: { value: "test" } });
+    fireEvent.change(input, { target: { value: "test" } });
 
-//     expect(input).toHaveDisplayValue("test");
+    expect(input).toHaveDisplayValue("test");
 
-//     // addButton.click();
-//     // expect(resultArea).toHaveTextContent("13");
-//   });
-// });
+    // addButton.click();
+    // expect(resultArea).toHaveTextContent("13");
+  });
+});
